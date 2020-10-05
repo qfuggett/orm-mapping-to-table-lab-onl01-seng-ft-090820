5ed40812726850908b94ac0011433d982d7b3005
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-05 18:43:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,8 +31,8 @@ class Student
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade, id),
-      VALUES (?, ?, ?);
+      INSERT INTO students (name, grade),
+      VALUES (?, ?);
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
     
