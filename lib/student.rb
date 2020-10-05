2ab5131e27301cddea6cb81e017073d28ee4c9c0
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-05 18:42:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,10 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE students(id INSERT PRIMARY KEY, name TEXT, grade INTEGER);
+      CREATE TABLE students (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      grade INTEGER);
     SQL
     DB[:conn].execute(sql)
   end
