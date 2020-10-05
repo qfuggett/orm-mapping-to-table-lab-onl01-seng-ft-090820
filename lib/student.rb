caa938f7fef1e4604b47cb52916956b9c0e5fa35
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-05 18:25:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,42 @@
 class Student
   attr_accessor :name, :grade
+  attr_reader :id
+  @@all = [ ]
   
   
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+    @@all
+  end
   
+  
+  def self
+    
+  end
+  
+  
+  def self.create_table
+    
+    
+  end
+  
+  
+  def self.drop_table
+    
+  end
+
+
+  def save
+    
+  end
+  
+  
+  def self.create
+    
+    
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
